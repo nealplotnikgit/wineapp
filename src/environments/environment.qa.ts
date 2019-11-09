3142160d04fa5f0cd1ec4a7be44a28cda34d3389
nealplotnikgit <nealplotnik@gmail.com>
2019-11-09 17:48:15 -0600

    ran in local pointing to pcf

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
-  wineServiceURL: 'http://winemanagementservice.cfapps.io',
+//  wineServiceURL: 'http://winemanagementservice.cfapps.io',
+  wineServiceURL: 'http://localhost:4200',
   // to build qa, https://blog.angularindepth.com/becoming-an-angular-environmentalist-45a48f7c20d8
 // https://medium.com/@balramchavan/separating-production-and-development-http-urls-using-environment-ts-file-in-angular-4c2dd0c5a8b0
 // ng build --configuration='qa'
-wineAppEnv: 'qa'
+wineAppEnv: 'qa-points to local server, proxies to pcf'
 };
